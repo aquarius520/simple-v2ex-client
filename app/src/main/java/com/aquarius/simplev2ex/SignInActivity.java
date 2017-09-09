417e245d8ee482361d377e30346b9e8d2c5285a5
aquarius <mengyuan0914@qq.com>
2017-09-09 22:52:27 +0800

    替换启动图标，测试机定制系统对桌面图标缓存了，导致修改图标没有替换成功，重启也不行，必须清除桌面的图标缓存数据

---
+++ app/src/main/java/com/aquarius/simplev2ex/SignInActivity.java
@@ -0,0 +1,34 @@
+package com.aquarius.simplev2ex;
+
+import android.content.Intent;
+
+/**
+ * Created by aquarius on 2017/9/9.
+ */
+public class SignInActivity extends BaseActivity {
+
+    @Override
+    protected void handleIntent(Intent intent) {
+
+    }
+
+    @Override
+    protected void inflateContentView() {
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void bindDataAndSetListeners() {
+
+    }
+
+    @Override
+    protected void requestData() {
+
+    }
+}
